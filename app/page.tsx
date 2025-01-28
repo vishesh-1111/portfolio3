419d23b1f77d9496c1715c8a127529f8eafd0a12
vishesh <visheshgautam.official@gmail.com>
2025-01-28 21:50:52 +0530

    3rd try

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
+    <main className="flex flex-col items-center px-4 ">
       <Intro />
        <About />
-
+        
       <Projects />
        <Contact />
      </main>
