40333dcd21dfbd01719396946f15a7fb3c657fb5
vishesh <visheshgautam.official@gmail.com>
2025-01-30 13:49:13 +0530

    changes

--- components/contact.tsx
+++ components/contact.tsx
@@ -39,7 +39,7 @@ export default function Contact() {
 
         <a
           className=" group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/resume (2).pdf"
+          href="/resume.pdf"
           download
           style={{maxWidth:'200px'}}
           >
