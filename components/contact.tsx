5674759f8d8d5c992e4399ac9242dfb74b478ee9
vishesh <visheshgautam.official@gmail.com>
2025-02-01 11:58:26 +0530

    minor fixes

--- components/contact.tsx
+++ components/contact.tsx
@@ -49,7 +49,7 @@ export default function Contact() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/vishesh-kumar-gautam-206216290"
           target="_blank"
           >
           <BsLinkedin />
@@ -57,7 +57,7 @@ export default function Contact() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/vishesh-1111"
           target="_blank"
           >
           <FaGithubSquare />
